c0a0b9c4f8c21172f497b1aa74d6d760543d8d38
TanglyEagle7718 <rishi.borra@outlook.com>
2024-02-10 09:26:18 -0500

    convertBase64toImage.py converts a base64 string to an image

--- MobileApp/flask-server/convertBase64toImage.py
+++ MobileApp/flask-server/convertBase64toImage.py
@@ -5,3 +5,12 @@ Created on Sat Feb 10 09:21:45 2024
 @author: rishi
 """
 
+with open('Output.txt', 'r') as file:
+    data = file.read().rstrip()
+    
+import pybase64
+decoded_data=pybase64.b64decode((data))
+#write the decoded data back to original format in  file
+img_file = open('image.png', 'wb')
+img_file.write(decoded_data)
+img_file.close()
